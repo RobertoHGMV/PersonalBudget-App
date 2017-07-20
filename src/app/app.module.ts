5de038468983645c44873e14db14de5500d7ecf0
Roberto <robertohgm@hotmail.com>
2017-07-20 01:10:58 -0300

    página de login e home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Root
 import { AppComponent } from './app.component';
+
+//Shared
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+//Pages
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
